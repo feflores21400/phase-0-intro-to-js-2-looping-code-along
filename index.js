c9bf601d3015351ad97d22e6ffb249039e71bc63
Fernando <feflores21400@gmail.com>
2022-09-11 16:48:49 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names =["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names){
+    const thanks =[]
+    for (let i = 0; i< names.length; i++){
+        thanks.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        
+    
+    }
+    return thanks;
+}
+writeCards(names);
+
+function countDown(){
+let countDown = 10
+while(countDown >= 0){
+    console.log(countDown--);
+}
+};
